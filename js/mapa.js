5b3ff80926b49903471360aa87508c2dea85c8e4
Daniel Barbosa de Souza <d.barbosa@academico.ifrn.edu.br>
2019-11-01 16:28:52 -0300

    Melhorias no codigo do mapa, ajustes na barra inferior de navegação e no menu

--- js/mapa.js
+++ js/mapa.js
@@ -32,32 +32,32 @@ class LocalControl {
 
         this.controlUI.addEventListener('click', () => {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function (position) { // callback de sucesso
-                    // ajusta a posiÃ§Ã£o do marker para a localizaÃ§Ã£o do usuÃ¡rio
-                    marker = new google.maps.Marker({
-                        map: map,
-                        draggable: false,
-                        animation: google.maps.Animation.DROP,
-                        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-                        icon: 'img/mapa/user.png'
-                    });
-                    infowindow = new google.maps.InfoWindow({
-                        content: 'Localiza��o'
-                    });
+                navigator.geolocation.getCurrentPosition(function(position) { // callback de sucesso
+                        // ajusta a posiçãoo do marker para a localizaÃ§Ã£o do usuário
+                        marker = new google.maps.Marker({
+                            map: map,
+                            draggable: false,
+                            animation: google.maps.Animation.DROP,
+                            position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                            icon: 'img/mapa/user.png'
+                        });
+                        infowindow = new google.maps.InfoWindow({
+                            content: 'Localização'
+                        });
 
-                    marker.addListener('click', function () {
-                        infowindow.open(map, marker);
-                    });
-                    map.panTo(marker.position);
-                    map.setZoom(16);
-                },
-                    function (error) { // callback de erro
+                        marker.addListener('click', function() {
+                            infowindow.open(map, marker);
+                        });
+                        map.panTo(marker.position);
+                        map.setZoom(16);
+                    },
+                    function(error) { // callback de erro
                         alert('Erro ao obter localização!');
                         console.log('Erro ao obter localização.', error);
                     });
                 this.controlDiv.style.display = 'none';
             } else {
-                console.log('Navegador nÃ£o suporta Geolocalização!');
+                console.log('Navegador não suporta Geolocalização!');
             }
         });
     }
@@ -69,14 +69,14 @@ function marcadores() {
 
     var marker, i;
 
-    $.getJSON('js/pontos.json', function (pontos) {
+    $.getJSON('js/pontos.json', function(pontos) {
 
-        $.each(pontos, function (index, pontos) {
+        $.each(pontos, function(index, pontos) {
 
             if (pontos.status == true) {
                 icon = 'img/mapa/valid.png';
             } else {
-                icon = 'img/mapa/not-valid.png';
+                icon = 'none';
             }
 
             var marker = new google.maps.Marker({
@@ -85,22 +85,176 @@ function marcadores() {
                 icon: icon
             });
 
-            google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                return function () {
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
                     infowindow.setContent(pontos.nome);
                     infowindow.open(map, marker);
                 };
             })(marker, i))
         });
     });
-    
+
 }
 
 function initMap() {
     const mapOptions = {
         center: centerMap,
         zoom: 13.5,
-        disableDefaultUI: true
+        disableDefaultUI: true,
+        styles: [{
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#d3d3d3"
+                }]
+            },
+            {
+                "featureType": "transit",
+                "stylers": [{
+                        "color": "#808080"
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#b3b3b3"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#ffffff"
+                    },
+                    {
+                        "weight": 1.8
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#d7d7d7"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#ebebeb"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#a7a7a7"
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#efefef"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#696969"
+                }]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#737373"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#d6d6d6"
+                }]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {},
+            {
+                "featureType": "poi",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#dadada"
+                }]
+            }
+        ]
     };
 
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
